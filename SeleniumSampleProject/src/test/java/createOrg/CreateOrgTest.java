2dac248a4c4bf96376e06ae7df43a7b55be77488
Renold Malhare <119624133+RenoldMalhare@users.noreply.github.com>
2026-05-07 09:58:08 +0530

    added data

--- SeleniumSampleProject/src/test/java/createOrg/CreateOrgTest.java
+++ SeleniumSampleProject/src/test/java/createOrg/CreateOrgTest.java
@@ -3,12 +3,17 @@ package createOrg;
 import org.testng.annotations.Test;
 
 public class CreateOrgTest {
-    @Test
-    public void createOrg() {
-    	   System.out.println("Org created");
-    }
-    @Test
-    public void createOrgWithAddress() {
- 	   System.out.println("Org created with Address");
-    }
+	@Test
+	public void createOrg() {
+		String product1 = System.getProperty("P1");
+		String product2 = System.getProperty("P2");
+		System.out.println(product1);
+		System.out.println(product2);
+		System.out.println("Org created");
+	}
+
+	@Test
+	public void createOrgWithAddress() {
+		System.out.println("Org created with Address");
+	}
 }
